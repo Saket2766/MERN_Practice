63f35f236c0281b9e273f2e2e2c70e89eec02831
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-07-13 20:17:48 +0530

    new file:   backend/controllers/userController.js 	new file:   backend/models/userModel.js 	modified:   backend/package-lock.json 	modified:   backend/package.json 	new file:   backend/routes/user.js 	modified:   backend/server.js 	modified:   frontend/public/index.html

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user')
 const mongoose = require('mongoose');
 const server = express();
 
@@ -15,6 +16,7 @@ server.use((req,res,next) => {
 
 //Routes
 server.use('/api/workouts',workoutRoutes);
+server.use('/api/user',userRoutes);
 
 //connect to DB
 mongoose.connect(process.env.MONG_URI)
